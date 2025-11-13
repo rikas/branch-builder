4bfca55ed60263d9a9cb3e07e902c609cf4f9dab
Ricardo Otero <oterosantos@gmail.com>
2025-11-13 11:21:22 +0000

    Fix disabling button

--- src/CreateTreeButton.tsx
+++ src/CreateTreeButton.tsx
@@ -40,7 +40,7 @@ export default function CreateTreeButton(): React.ReactElement {
     close();
   };
 
-  const isDisabled = treeData.items.length === 1;
+  const isDisabled = getRootChildren(treeData).length === 0;
 
   return (
     <DialogTrigger>
